36c156494cf6bdd71c4044b58df94e5da0d46da8
liwateryi <liwateryi@163.com>
2016-09-21 20:42:46 +0800

    mark

--- DataHelp/ConnectionString.cs
+++ DataHelp/ConnectionString.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DeepSeaSafe;
 
 namespace DataHelp
 {
     public class ConnectionString
     {
+        private static DeepSeaSafe.DeepSeaSafe safe = new DeepSeaSafe.DeepSeaSafe();
         /// <summary>
         ///sqlserver 链接示例:server=.;uid=sa;pwd=123;database=DemoDB;/>
         // mysql 链接示例[必须加上oldsyntax=true，以便支付mysql老语法，参数化命令时会用到@]
@@ -16,9 +18,15 @@ namespace DataHelp
         {
             get
             {
-                return System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                if (Convert.ToBoolean(System.Configuration.ConfigurationManager.AppSettings["IsEncryption"] ?? "false"))
+                {
+                    return safe.DecrypWebConfig(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                }
+                else
+                {
+                    return System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                }
             }
         }
-
     }
 }
